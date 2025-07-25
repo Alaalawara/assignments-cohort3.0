b53a2aed13fa3f917d2ed0a4a69871a25fcf17a4
Alaalawara <you@example.com>
2025-07-25 17:44:57 +0530

    week-2-async-hard solve

--- week-2/week-2-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/week-2-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,13 @@
 */
 
 function wait(n) {
+
+    let pro= new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },n*1000);
+    });
+    return pro;
 }
 
 module.exports = wait;
